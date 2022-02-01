763837cdc43b26de14c7c662b02e3cddb4886879
Mario Poerner <Mario.Poerner@adidas-group.com>
2022-02-01 08:46:39 +0100

    solved

--- aoc24.exs
+++ aoc24.exs
@@ -1,4 +1,7 @@
 defmodule AOC24 do
+  # Lowest model number: 13621111481315
+  # Highest model number: 59998426997979
+
   def parse(source_code) do
     source_code
     |> String.split("\n")
@@ -63,12 +66,7 @@ defmodule AOC24 do
     %{state | a => eql}
   end
 
-  def evaluate(_, _, state, 0) do
-    state
-  end
-
-  def evaluate([instruction | rest], input, state, instruction_count \\ 18) do
-    #IO.inspect(state.z, label: "PROCESSING INSTRUCTION")
+  def evaluate([instruction | rest], input, state) do
     {input, state} = case instruction do
       {:inp, [a]} -> inp(a, input, state)
       {:add, [a,b]} -> {input, add(a, b, state)}
@@ -77,68 +75,56 @@ defmodule AOC24 do
       {:mod, [a,b]} -> {input, mod(a, b, state)}
       {:eql, [a,b]} -> {input, eql(a, b, state)}
     end
-    #if rem(instruction_count, 18) == 0, do: IO.puts("Z: #{state.z}")
-    evaluate(rest, input, state, instruction_count - 1)
+
+    evaluate(rest, input, state)
   end
 
-  def evaluate([], _, state, _) do
+  def evaluate([], _, state) do
     state
   end
 
-  def solve_3(source_code) do
-    program = parse(source_code)
-    initial_state = %{:x => 0, :y => 0, :z => 0, :w => 0}
-    Enum.each(1000..0, fn number ->
+  def possible_z_states(program, constraints, subprogram_index \\ 13)
+
+  def possible_z_states(_program, constraints, -1) do
+    constraints
+  end
+
+  def possible_z_states(program, constraints, subprogram_index) do
+    # We start at the last instruction block and figure out which z-state leads to a 0 output
+    # The we go through the instruction blocks one by one and ensure the z-states of the output match one possible z-state of the input.
+    # along the way we remember the inputs in every stage so we can reassemble the final number
+    # To get the smallest number just reverse the 1..9 range
+    subprogram = Enum.slice(program, subprogram_index*18, 18)
+    possible_numbers = Enum.reduce(245113..0, %{}, fn number, possible_numbers ->
       initial_state = %{:x => 0, :y => 0, :z => number, :w => 0}
-      state = evaluate(program, [9], initial_state, 18)
-      #state = evaluate(program, [9,9,9,9,9,9,9,9,9,9,9,9,9,9], initial_state, 14*18)
-      #if state.z == 0, do: IO.puts("#{number}: #{state.z}")
-      IO.puts("#{number}: #{state.z}")
-    end)
-  end
-
-  def solve_2(source_code) do
-    program = parse(source_code)
-    initial_state = %{:x => 0, :y => 0, :z => 0, :w => 0}
-    Enum.each(9..1, fn number ->
-      state = evaluate(program, [9,3,9,9,8,4,1,1,4,8,1,3,1,1], initial_state, 14*18)
-      #state = evaluate(program, [9,9,9,9,9,9,9,9,9,9,9,9,9,9], initial_state, 14*18)
-      IO.puts("#{number}: #{state.z}\n")
-    end)
-  end
-
-  def solve(source_code) do
-    program = parse(source_code)
-    initial_state = %{:x => 0, :y => 0, :z => 0, :w => 0}
-
-    table = :ets.new(:min_value, [:set, :protected])
-    :ets.insert(table, {:minimum, 9999999999999})
-
-    Enum.each(99999999999999..11111111111111, fn number ->
-      number = Integer.to_charlist(number) |> Enum.map(fn i -> i - 48 end)
-      #number = number ++ [1,1,1,1,1,1,1,1]
-      if Enum.all?(number, fn n -> n > 0 end) do
-        #state = evaluate(program, [1,3,9,9,8,4,1,1,4,8,1,3,1,9],initial_state)
-        state = evaluate(program, number,initial_state, 14*18)
-        #IO.inspect({number, state.z})
-        [{:minimum, minimum}] = :ets.lookup(table, :minimum)
-
-        if state.z < minimum do
-          :ets.insert(table, {:minimum, state.z})
-          IO.puts("NEW MINIMUM #{state.z}")
-          #Process.sleep(1000)
+      Enum.reduce(1..9, possible_numbers, fn input, possible_numbers ->
+        state = evaluate(subprogram, [input], initial_state)
+        # Check if current z register state is one of the required states of previous stage
+        case Map.fetch(constraints, state.z) do
+          {:ok, value} -> Map.put(possible_numbers, number, [input | value])
+          :error -> possible_numbers
         end
-      end
+      end)
     end)
+
+    IO.puts("#{subprogram_index}, #{map_size(possible_numbers)} #{inspect possible_numbers}")
+    possible_z_states(program, possible_numbers, subprogram_index - 1)
+  end
+
+  def is_valid_model_number(source_code, number) do
+    program = parse(source_code)
+    initial_state = %{:x => 0, :y => 0, :z => 0, :w => 0}
+    state = evaluate(program, number, initial_state)
+    state.z == 0
   end
 end
 
-input_a = "inp z
+_input_a = "inp z
 inp x
 mul z 3
 eql z x"
 
-input_b = "inp w
+_input_b = "inp w
 add z w
 mod z 2
 div w 2
@@ -150,7 +136,7 @@ mod x 2
 div w 2
 mod w 2"
 
-input = "inp w
+input_full = "inp w
 mul x 0
 add x z
 mod x 26
@@ -403,160 +389,5 @@ add y 7
 mul y x
 add z y"
 
-input_0 = "inp w
-mul x 0
-add x z
-mod x 26
-div z 1
-add x 13
-eql x w
-eql x 0
-mul y 0
-add y 25
-mul y x
-add y 1
-mul z y
-mul y 0
-add y w
-add y 8
-mul y x
-add z y"
-
-input_1 = "inp w
-mul x 0
-add x z
-mod x 26
-div z 1
-add x 12
-eql x w
-eql x 0
-mul y 0
-add y 25
-mul y x
-add y 1
-mul z y
-mul y 0
-add y w
-add y 13
-mul y x
-add z y"
-
-input_2 = "inp w
-mul x 0
-add x z
-mod x 26
-div z 1
-add x 12
-eql x w
-eql x 0
-mul y 0
-add y 25
-mul y x
-add y 1
-mul z y
-mul y 0
-add y w
-add y 8
-mul y x
-add z y"
-
-input_3 = "inp w
-mul x 0
-add x z
-mod x 26
-div z 1
-add x 10
-eql x w
-eql x 0
-mul y 0
-add y 25
-mul y x
-add y 1
-mul z y
-mul y 0
-add y w
-add y 10
-mul y x
-add z y"
-
-input_11 = "inp w
-mul x 0
-add x z
-mod x 26
-div z 1
-add x 14
-eql x w
-eql x 0
-mul y 0
-add y 25
-mul y x
-add y 1
-mul z y
-mul y 0
-add y w
-add y 2
-mul y x
-add z y"
-
-input_12 = "inp w
-mul x 0
-add x z
-mod x 26
-div z 26
-add x 0
-eql x w
-eql x 0
-mul y 0
-add y 25
-mul y x
-add y 1
-mul z y
-mul y 0
-add y w
-add y 2
-mul y x
-add z y"
-
-input_13 = "inp w
-mul x 0
-add x z
-mod x 26
-div z 26
-add x -15
-eql x w
-eql x 0
-mul y 0
-add y 25
-mul y x
-add y 1
-mul z y
-mul y 0
-add y w
-add y 12
-mul y x
-add z y"
-
-input_14 = "inp w
-mul x 0
-add x z
-mod x 26
-div z 26
-add x -4
-eql x w
-eql x 0
-mul y 0
-add y 25
-mul y x
-add y 1
-mul z y
-mul y 0
-add y w
-add y 7
-mul y x
-add z y"
-
-IO.inspect(AOC24.solve_3(input_11), label: "INPUT")
-#IO.inspect(AOC24.solve(input_0), label: "INPUT 0")
-#IO.inspect(AOC24.solve(input_1), label: "INPUT 1")
-#IO.inspect(AOC24.solve(input_2), label: "INPUT 2")
-#IO.inspect(AOC24.solve(input_3), label: "INPUT 3")
+IO.inspect(AOC24.is_valid_model_number(input_full, [5, 9, 9, 9, 8, 4, 2, 6, 9, 9, 7, 9, 7, 9]), label: "HIGHEST MODEL NUMBER IS VALID")
+IO.inspect(AOC24.possible_z_states(AOC24.parse(input_full), %{0 => []}), label: "INPUT")
